47da7f40e2100940c507a02dd57e7243c6b7f086
camilaadriano <camilaadrianocoelho@gmail.com>
2021-12-20 22:34:56 -0300

    Agencia de Viagem - Atividade Prática

---
+++ Back-end/Atividade Prática C#.net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Atividade_Prática_C_.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            String Email;
+            String Comentario;
+
+            Console.WriteLine("Formulário para Contato");
+            Console.WriteLine("Digite seu nome");
+            nome= Console.ReadLine();
+            Console.WriteLine("Digite seu Email");
+            Email= Console.ReadLine();
+            Console.WriteLine("Digite seu Comentário");
+            Comentario= Console.ReadLine();
+
+        }
+    }
+}
